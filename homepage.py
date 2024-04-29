415856e58d0f4ddb755b8f03861d5f8513c68075
Sanjay <133491977+0xSanjayKumar@users.noreply.github.com>
2024-04-29 23:30:06 +0500

    Update homepage.py

--- homepage.py
+++ homepage.py
@@ -13,3 +13,12 @@ st.image(image, use_column_width=True)
 st.markdown("Powered by data from TripAdvisor, our app curates recommendations from 20 cities across New York, New Jersey, California, Texas, and Washington. Find the top 10 restaurants similar to your favorites.")
 st.markdown("Leveraging Natural Language Processing and Content-Based Recommender Systems, we prioritize user comments to deliver personalized suggestions.")
 st.success("Satisfy your cravings with ease! :fork_and_knife:" ":yum:")
+
+
+st.py_run("Restaurant.py")
+
+# Run second Python file
+st.py_run("Recommended.py")
+
+# Run third Python file
+st.py_run("Dasboard.py")
