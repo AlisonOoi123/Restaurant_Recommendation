fa632f54555f3b506da20cd913308874a2fbce93
Sanjay <133491977+0xSanjayKumar@users.noreply.github.com>
2024-04-29 21:32:03 +0500

    Update homepage.py

--- homepage.py
+++ homepage.py
@@ -7,9 +7,9 @@ st.set_page_config(layout='centered', initial_sidebar_state='expanded')
 
 st. sidebar.image('Data/App_icon.png')
 
+st.success("Discover the best places to eat near you!")
 image = Image.open('Data/Food.png')
 st.image(image, use_column_width=True)
-st.success("Discover the best places to eat near you!")
 st.markdown("Powered by data from TripAdvisor, our app curates recommendations from 20 cities across New York, New Jersey, California, Texas, and Washington. Find the top 10 restaurants similar to your favorites.")
 st.markdown("Leveraging Natural Language Processing and Content-Based Recommender Systems, we prioritize user comments to deliver personalized suggestions.")
 st.success("Satisfy your cravings with ease! :fork_and_knife:" ":yum:")
