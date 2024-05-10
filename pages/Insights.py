8e85f6325d3e7bd404549840c78b892be3a22a62
Sanjay <133491977+0xSanjayKumar@users.noreply.github.com>
2024-05-10 21:49:33 +0500

    Update Insights.py

--- pages/Insights.py
+++ pages/Insights.py
@@ -38,6 +38,8 @@ types_counts.plot(kind='pie', shadow=True, cmap=plt.get_cmap('Spectral'), ax=ax)
 ax.set_ylabel('')
 ax.tick_params(colors='white')
 ax.title.set_color('white')
+plt.xticks(rotation=45, color='white')  
+plt.yticks(rotation=45, color='white')  
 
 with col2:
     st.markdown("""
