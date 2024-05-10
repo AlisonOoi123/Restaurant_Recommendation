7c595d3dd6da80b5a8b68d9b7652efc3c751858b
Sanjay <133491977+0xSanjayKumar@users.noreply.github.com>
2024-05-10 21:17:18 +0500

    Update Insights.py

--- pages/Insights.py
+++ pages/Insights.py
@@ -33,8 +33,10 @@ fig.set_facecolor('#121212')
 ax.set_facecolor('#121212')
 
 types_counts.plot(kind='pie', shadow=True, cmap=plt.get_cmap('Spectral'), ax=ax)
+sns.barplot(x='City', y="weighted_ratings", data=types_counts, palette="flare", ax=ax)
+
 ax.set_ylabel('Cuisine Types', color='white')
-ax.set_ylabel('Vegetarian Friendly', color='white')
+ax.set_xlabel('Vegetarian Friendly', color='white')
 ax.tick_params(colors='white')
 ax.title.set_color('white')
 with col2:
